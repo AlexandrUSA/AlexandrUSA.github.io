57c364b427ff7c45ec1f3e31c85c33c1bab29257
AlexandrUSA <AlexandrUSA@yandex.ru>
2019-06-19 23:41:50 +0300

    v2

--- sw.js
+++ sw.js
@@ -3,37 +3,42 @@ self.importScripts('./cache-polyfill.js');
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/assets/favicon.ico',
-        '/assets/app.css',
-        '/assets/img/about-pic.webp',
-        '/assets/img/about-pic.png',
-        '/assets/img/car1.webp',
-        '/assets/img/car1.jpeg',
-        '/assets/img/bg.jpeg',
-        '/assets/img/borispol.jpg',
-        '/assets/src/fonts/iconfont/MaterialIcons-Regular.eot',
-        '/assets/src/fonts/iconfont/MaterialIcons-Regular.ttf',
-        '/assets/src/fonts/iconfont/MaterialIcons-Regular.woff',
-        '/assets/src/fonts/iconfont/MaterialIcons-Regular.woff2',
-        '/assets/src/fonts/iconfont/MaterialIcons-Regular.svg',
-        '/assets/icons/android-chrome-192x192',
-        '/assets/icons/android-chrome-512x512',
-        '/assets/icons/apple-touch-icon',
-        '/assets/icons/apple-touch-icon-60x60',
-        '/assets/icons/apple-touch-icon-76x76',
-        '/assets/icons/apple-touch-icon-120x120',
-        '/assets/icons/apple-touch-icon-152x152',
-        '/assets/icons/apple-touch-icon-180x180',
-        '/assets/icons/favicon-16x16',
-        '/assets/icons/favicon-32x32',
-        '/assets/icons/msapplication-icon-144x144',
-        '/assets/icons/mstile-150x150',
-        '/js/app.js',
-        '/js/1.chunk.js',
-      ]);
+      try {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/assets/favicon.ico',
+          '/assets/1.app.css',
+          '/assets/app.css',
+          '/assets/img/about-pic.webp',
+          '/assets/img/about-pic.png',
+          '/assets/img/car1.webp',
+          '/assets/img/car1.jpeg',
+          '/assets/img/bg.jpeg',
+          '/assets/img/borispol.jpg',
+          '/assets/src/fonts/iconfont/MaterialIcons-Regular.eot',
+          '/assets/src/fonts/iconfont/MaterialIcons-Regular.ttf',
+          '/assets/src/fonts/iconfont/MaterialIcons-Regular.woff',
+          '/assets/src/fonts/iconfont/MaterialIcons-Regular.woff2',
+          '/assets/src/fonts/iconfont/MaterialIcons-Regular.svg',
+          '/assets/icons/android-chrome-192x192.png',
+          '/assets/icons/android-chrome-512x512.png',
+          '/assets/icons/apple-touch-icon.png',
+          '/assets/icons/apple-touch-icon-60x60.png',
+          '/assets/icons/apple-touch-icon-76x76.png',
+          '/assets/icons/apple-touch-icon-120x120.png',
+          '/assets/icons/apple-touch-icon-152x152.png',
+          '/assets/icons/apple-touch-icon-180x180.png',
+          '/assets/icons/favicon-16x16.png',
+          '/assets/icons/favicon-32x32.png',
+          '/assets/icons/msapplication-icon-144x144.png',
+          '/assets/icons/mstile-150x150.png',
+          '/js/app.js',
+        ]);
+      } catch (e) {
+        console.error(e);
+        return null;
+      }
     }),
   );
 });
